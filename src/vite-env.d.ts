30c9f53ce2716f84b160b03182ad93a4b108d9f7
BoraCume <dev@boracume.local>
2026-03-10 17:47:57 -0300

    chore: ajustar tipos env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,3 +3,11 @@
 interface Window {
   webkitAudioContext?: typeof AudioContext;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_MAPS_BROWSER_API_KEY?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
